fd4f1f49d8fbd7fe849b425e749e960164587440
AdvaOhana <adva1230@gmail.com>
2024-08-12 02:53:08 +0300

    התחלת קוד
    הגדרה של כפתורים, לדים, באזר ואורך של מערכים שבהמשך יהיה בהם שימוש

---
+++ FocusGame.ino
@@ -0,0 +1,23 @@
+#define blueLed 11
+#define greenLed 10
+#define yellowLed 9
+#define redLed 6
+
+#define blueBtn 12
+#define greenBtn 2
+#define yellowBtn 3
+#define redBtn 4
+
+#define LEN 4
+#define MAX_NUM_OF_LIGHTS 3
+#define BuzzTone 5
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
